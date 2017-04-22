eda32ccd1a2691b85348d3b3cab85c6e2ebd082a
bknguyen <bknguyen@sopragroup.com>
2017-04-22 18:26:36 +0200

    refactor helpers + Part 2 + Part 3

--- RProgramming-W2-ProgrammingAssignment.R
+++ RProgramming-W2-ProgrammingAssignment.R
@@ -6,13 +6,26 @@ checkAgument <- function (d, p, i) {
 }
 
 #Read file helper
+readFile <- function(directory, iMonitor) {
+  dfI <- read.csv (file.path(directory,paste(str_pad(iMonitor,3,side="left","0"),".csv", sep="")), header = TRUE)
+  dfI
+}
+
 # read file into a datafame excludinf NA values
 readDataFrame <- function(directory, pollutant, iMonitor) {
-  dfI <- read.csv (file.path(directory,paste(str_pad(iMonitor,3,side="left","0"),".csv", sep="")), header = TRUE)
+  dfI <- readFile(directory, iMonitor)
   # compute mean of pollutant excluding NA value
   dfI[!is.na(dfI[,pollutant]),pollutant]
 }
 
+readCompleteDataFrame <- function(directory, iMonitor) {
+  dfI <- readFile(directory, iMonitor)
+  # compute mean of pollutant excluding NA value
+  dfReturn <- dfI[!is.na(dfI[,"nitrate"]) & !is.na(dfI[,"sulfate"]),]
+  dfReturn
+}
+
+# Part 1
 # Main function : computing weighted accross monitors specified in id argument
 # algo : as we cannot load all 332 file in memory, we compute mean value file by file and
 # compute final mean of all these means.
@@ -42,4 +55,43 @@ pollutantmean <- function(directory, pollutant, id = 1:332) {
   }
 }
 
+# Part 2
+# Main function
+complete <- function(directory, id = 1:332){
+  if (!dir.exists(directory)  | length(id) > 332) {
+    print("Error")
+    return
+  }
+  #Init dataframe
+  dataframeComplete <- NULL
+  # append the number of observations of each monitor
+  for (i in id) {
+    dfI <- readCompleteDataFrame(directory, i)
+    dataframeComplete<-rbind(dataframeComplete, c(i, nrow(dfI)))
+  }
+  #Naming the colums of the result
+  colnames(dataframeComplete) <- c("id", "nobs")
+  #Naming the rows of the result
+  dataframeComplete <- data.frame(dataframeComplete, row.names = seq(1:length(id)))
+  
+  #return dataframe
+  dataframeComplete
+}
 
+#Part 3
+corr <- function(directory, threshold = 0) {
+  dfComplete <- complete(directory)
+  dfCompleteThreshold <- dfComplete[dfComplete[,"nobs"] > threshold,]
+  correlations <- as.numeric(NULL)
+  if (nrow(dfCompleteThreshold) > 0) {
+    for (i in 1:nrow(dfCompleteThreshold)) {
+      idMonitor = dfCompleteThreshold[i,"id"]
+      dfI <- readFile(directory, idMonitor)
+      varSulfate <- dfI[,"sulfate"]
+      varNitrate <- dfI[,"nitrate"]
+      correlation <- cor(varNitrate, varSulfate, use = "na.or.complete")
+      correlations <- cbind(correlations, correlation)
+    }
+  }
+  as.numeric(correlations)
+}
